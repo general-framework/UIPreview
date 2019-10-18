058226ace4de76b5e9926d530ee4fee675cbdd0d
j-j-m <martinjaco@gmail.com>
2019-10-18 18:59:41 -0400

    fix test folder structure

---
+++ Tests/UIPreviewTests/UIPreviewTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import PrieviewExperiments
+
+final class UIPreviewTests: XCTestCase {
+    func testExample() {
+        //TODO: - implement test
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
